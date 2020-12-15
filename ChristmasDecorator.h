774b8fa3c4644d0626fe103902ed2c93936b9e05
christophehad <50202327+christophehad@users.noreply.github.com>
2020-12-15 10:00:59 +0100

    added a range for the windows labels color

--- ChristmasDecorator.h
+++ ChristmasDecorator.h
@@ -12,5 +12,8 @@ namespace CD {
 	const string inputLabelsPath = srcDir + "/data/cmp_b0377.png";
 
 	// colors
-	const Vec3b windowColor = { 255, 85, 0 };
+	//const Vec3b windowColor = { 255, 85, 0 };
+	const Vec3b windowColor = { 250, 121, 6 };
+	const Vec3b windowColorLower = { 245, 80, 0 };
+	const Vec3b windowColorUpper = { 255, 130, 10 };
 }
